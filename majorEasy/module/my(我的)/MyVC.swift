576dbf692a6c456c6744c3cc351eb20cada0f64d
dedewang <wangyang@tanchengiot.com>
2020-09-29 15:45:13 +0800

    我的页面编码

--- majorEasy/module/my(我的)/MyVC.swift
+++ majorEasy/module/my(我的)/MyVC.swift
@@ -2,20 +2,33 @@
 //  MyVC.swift
 //  majorEasy
 //
-//  Created by wangyang on 2020/9/24.
+//  Created by wangyang on 2020/9/29.
 //
 
 import UIKit
 
 class MyVC: BaseVC {
-    
 
+    @IBOutlet weak var topView: UIView!
+    @IBOutlet weak var moneyView: UIView!
+    @IBOutlet weak var messageView: UIView!
+    @IBOutlet weak var toolView: UIView!
+    @IBOutlet weak var headerView: UIView!
+    
     override func setUpData() {
+        
     }
     
     override func setUpView() {
-        self.navigationItem.title = "my".localized
-        createEmptyResultUI()
+        headerView.layer.cornerRadius = 30
+        self.view.backgroundColor = RGBHex(0xF6F6F6)
+        self.hideNavigationBar = true
+        self.moneyView.addShadow(RGBHex(0xe2e2e2))
+        self.messageView.addShadow(RGBHex(0xe2e2e2))
+        self.toolView.addShadow(RGBHex(0xe2e2e2))
+        topView.snp.updateConstraints { (make) -> Void in
+            make.height.equalTo(162+StatusHeight)
+        }
     }
 
 }
